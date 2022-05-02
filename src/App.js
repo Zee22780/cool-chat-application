de8193935b40ff44573c0baf6298308e89783693
zuri227 <zuri22780@gmail.com>
2022-05-02 19:47:57 -0400

    Install ant icons, axios, and react-chat-engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import React from 'react';
+import { ChatEngine } from 'react-chat-engine';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <ChatEngine
+			height='100vh'
+			userName='ava'
+			userSecret='hello'
+			projectID='46a280f0-3668-4d8e-a4cf-1c5438278369'
+		/>
     </div>
   );
 }
