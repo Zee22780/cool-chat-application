b32b50b1decd18cf5c4582a660ba672f1bb230a0
zuri227 <zuri22780@gmail.com>
2022-05-04 21:17:52 -0400

    Update project details in app.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { ChatEngine } from 'react-chat-engine';
 import './App.css';
+import ChatFeed from './components/ChatFeed';
 
 function App() {
   return (
     <div className="App">
       <ChatEngine
 			height='100vh'
-			userName='ava'
-			userSecret='hello'
-			projectID='46a280f0-3668-4d8e-a4cf-1c5438278369'
-		/>
+			userName='olivia'
+			userSecret='thankyou'
+			projectID='98816256-be79-4584-8848-480757fe7fcb'
+      renderChatFeed = { (chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage = { () => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+		  />
     </div>
   );
 }
