19658528f57dc8f4e1b690a492b378678c5f5481
zuri227 <zuri22780@gmail.com>
2022-05-04 22:29:11 -0400

    Fix issue with node modules

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React from 'react';
 import { ChatEngine } from 'react-chat-engine';
 import './App.css';
 import ChatFeed from './components/ChatFeed';
 
 function App() {
+
   return (
-    <div className="App">
       <ChatEngine
 			height='100vh'
 			userName='olivia'
@@ -14,7 +13,6 @@ function App() {
       renderChatFeed = { (chatAppProps) => <ChatFeed {...chatAppProps} />}
       onNewMessage = { () => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
 		  />
-    </div>
   );
 }
 
